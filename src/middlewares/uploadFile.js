6e7703df93673eb6b78db30c6e49b0498643f8f9
WillyAgustinoEfendi <59289325+Pentorch@users.noreply.github.com>
2022-03-09 20:25:06 +0700

    update

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,33 +1,36 @@
 const multer = require("multer");
- 
-exports.uploadFile = (imageFile) => {
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads");
-    },
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-    filename: function (req, file, cb) {
-      cb(null, `${Date.now()}-${file.originalname.replace(/\s/g, "")}`);
+exports.uploadFile = (fieldName) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
+
+  const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: async (req, file) => {
+      return {
+        folder: "uploads",
+        public_id: Date.now() + "-" + file.originalname,
+      };
     },
   });
 
   const fileFilter = function (req, file, cb) {
-    if (file.fieldname === imageFile) {
-      if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-        req.fileValidationError = {
-          message: "Only image files are accepted",
-        };
-
-        return cb(new Error("Only image files are accepted"), false);
-      }
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|svg|SVG)$/)) {
+      req.fileValidationError = {
+        message: "Only image files are allowed!",
+      };
+      return cb(new Error("Only image files are allowed!"), false);
     }
+
     cb(null, true);
   };
 
-  const sizeInMB = 10;
-  const maxSize = sizeInMB * 1000 * 1000;
-
-  // generate multer instance for upload
+  const maxSize = 10 * 1024 * 1024;
 
   const upload = multer({
     storage,
@@ -35,7 +38,7 @@ exports.uploadFile = (imageFile) => {
     limits: {
       fileSize: maxSize,
     },
-  }).single(imageFile);
+  }).single(fieldName);
 
   return (req, res, next) => {
     upload(req, res, function (err) {
@@ -43,20 +46,26 @@ exports.uploadFile = (imageFile) => {
         return res.status(400).send(req.fileValidationError);
       }
 
-      if (!req.file && !err) {
-        return res.status(400).send({
-          message: "Please select files to upload",
-        });
-      }
-
       if (err) {
         if (err.code === "LIMIT_FILE_SIZE") {
           return res.status(400).send({
-            message: "Max file size is 10MB",
+            message: `Max file sized exceeded (${maxSize} MB)`,
+          });
+        }
+
+        if (err.code === "LIMIT_UNEXPECTED_FILE") {
+          return res.status(400).send({
+            message: `Only single file allowed`,
           });
         }
 
         return res.status(400).send(err);
+      }
+
+      if (!req.file) {
+        return res.status(400).send({
+          message: "Please select a file to upload",
+        });
       }
 
       return next();
